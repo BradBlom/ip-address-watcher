429260074ee05bf3f13a89bed6d7916a70f258d3
BradBlom <bradley.j.blom@gmail.com>
2019-07-20 15:55:44 -0500

    Adding config files and making email send synchronous

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+const config = require('config');
 const fs = require('fs');
 const http = require('http');
 const uuidv4 = require('uuid/v4');
 const request = require('request');
 const nodemailer = require('nodemailer');
 
-const jobIpAddress = '192.168.0.20';
-const jobPortNum = 3000;
+const jobIpAddress = config.get('app-main.listen-address');
+const jobPortNum = config.get('app-main.listen-port');
 const jobId = 'd8980d51-e334-4397-ad05-8029a8736440';
 const jobName = 'ip-address-watcher'
 const ipApiUrl = 'https://api.ipify.org/?format=json';
@@ -22,32 +23,52 @@ function findExternalIp() {
 	});
 }
 
-function sendNotificationOfIpChange(newIp) {
-	var mailOptions = {
-		from: '',
-		to: '',
-		subject: 'Home IP Address Has Changed',
-		text: 'New IP address is: '+ newIp
-	};
-	
-	nodemailer.createTransport({
-		service: 'gmail',
-		auth: {
-			user: '',
-			pass: ''
-		}
-	}).sendMail(mailOptions, function(error, info){
-		if (error) {
-		console.log(error);
-		} else {
-		console.log('Email sent: ' + info.response);
-		}
+function sendEmailSync(transport, mailOptions) {
+	return new Promise((resolve, reject) => {
+		transport.sendMail(mailOptions, (err, info) => {
+			if (err) {
+				console.log("error: ", err);
+				reject(err);
+			} else {
+				console.log('Email sent: ' + info.response);
+				resolve(info);
+			}
+		});
 	});
 }
 
+async function sendNotificationOfIpChange(newIp) {
+	var alertMedium = config.get('app-main.alert-medium');
+	
+	var sendAlertIsSuccess = false;
+	
+	if (alertMedium == 'email') {
+		var mailOptions = {
+			from: config.get('email-configs.from-email'),
+			to: config.get('email-configs.to-email'),
+			subject: 'Home IP Address Has Changed',
+			text: 'New IP address is: '+ newIp
+		};
+		
+		var transport = nodemailer.createTransport({
+			service: config.get('email-configs.email-service'),
+			auth: {
+				user: config.get('email-configs.email-username'),
+				pass: config.get('email-configs.email-password')
+			}
+		});
+		
+		var emailResponse = await sendEmailSync(transport, mailOptions);
+		
+		console.log(JSON.stringify(emailResponse));
+		sendAlertIsSuccess = true;
+	}
+	
+	return Boolean(sendAlertIsSuccess);
+}
+
 let app = http.createServer(async (req, res) => {
-	//if (req.method != 'POST' || req.url != '/ip-address-watcher/start-job') {
-	if (req.url != '/ip-address-watcher/start-job') {
+	if (req.method != 'POST' || req.url != '/ip-address-watcher/start-job') {
 		console.log('A request was rejected for ' + req.method + ' ' + req.url);
 		res.writeHead(404);
 		res.end();
@@ -59,6 +80,7 @@ let app = http.createServer(async (req, res) => {
 	var startTime = new Date();
 	var runId = uuidv4();
 	var needsAlertSent = false;
+	var successfulAlertWasSent = false;
 	
 	// find external ip synchronously
 	var ipApiInfo = JSON.parse(await findExternalIp());
@@ -67,22 +89,23 @@ let app = http.createServer(async (req, res) => {
 	var pathLastIpAddr = process.cwd() + '/data/lastIpAddr';
 	
 	if (!fs.existsSync(pathLastIpAddr)) {
-		//console.log('Temp file lastIpAddr doesn\'t exist. Alerting and creating one');
-		fs.writeFileSync(pathLastIpAddr, ipApiInfo.ip);
-		sendNotificationOfIpChange(ipApiInfo.ip);
 		needsAlertSent = true;
 	} else {
 		var contents = fs.readFileSync(pathLastIpAddr);
 		if (contents == '') {
-			//console.log('Temp file lastIpAddr is empty. Alerting and updating file');
-			fs.writeFileSync(pathLastIpAddr, ipApiInfo.ip);
-			sendNotificationOfIpChange(ipApiInfo.ip);
 			needsAlertSent = true;
 		} else if (contents != ipApiInfo.ip) {
-			//console.log('Temp file lastIpAddr has an old ip (old: '+ contents + ', new: ' + ipApiInfo.ip + '). Alerting and updating file');
-			fs.writeFileSync(pathLastIpAddr, ipApiInfo.ip);
-			sendNotificationOfIpChange(ipApiInfo.ip);
 			needsAlertSent = true;
+		}
+	}
+	
+	if (needsAlertSent) {
+		fs.writeFileSync(pathLastIpAddr, ipApiInfo.ip);
+		try {
+			successfulAlertWasSent = await sendNotificationOfIpChange(ipApiInfo.ip);
+		}
+		catch(err) {
+			successfulAlertWasSent = false;
 		}
 	}
 	
@@ -90,17 +113,31 @@ let app = http.createServer(async (req, res) => {
 	var endTime = new Date();
 	var executionTime = hrend[0].toString() + 's ' + (hrend[1]/1000000).toString() + 'ms'
 	
+	var jobStatus;
+	var jobStatusMessage;
+	if (needsAlertSent && successfulAlertWasSent) {
+		jobStatus = 'success';
+		jobStatusMessage = 'Alert was successfully sent';
+	} else if (needsAlertSent && !successfulAlertWasSent) {
+		jobStatus = 'fail';
+		jobStatusMessage = 'Alert failed to be sent';
+	} else {
+		jobStatus = 'success';
+		jobStatusMessage = 'No alerts are necessary';
+	}
+	
 	var jobResults = {
 		"jobId": jobId,
 		"jobName": jobName,
 		"runId": runId,
-		"status": "success",
+		"status": jobStatus,
+		"statusMessage": jobStatusMessage,
 		"startTime": startTime.toISOString(),
 		"endTime": endTime.toISOString(),
 		"executionTime": executionTime,
 		"data": {
 			"externalIp": ipApiInfo.ip,
-			"alertWasSent": needsAlertSent
+			"alertWasSent": successfulAlertWasSent
 		}
 	};
 	
