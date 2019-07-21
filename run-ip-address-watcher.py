b9677a5c2f18c8be74c34a46516175d2f3145401
BradBlom <bradley.j.blom@gmail.com>
2019-07-20 20:33:23 -0500

    renaming the main class. Making the python script more generic and adding comments

--- run-ip-address-watcher.py
+++ run-ip-address-watcher.py
@@ -1,13 +1,17 @@
+# This file is a very basic demonstration of how to use this job.
+# This file can be run on a schedule with cron
 import requests
 
-jobListener = 'http://192.168.0.20:3000/ip-address-watcher/start-job'
+# modify these variables to suit your environment
+strJobListener = 'http://192.168.0.20:3000/ip-address-watcher/start-job'
+strLogLocation = '/home/pi/share/ip-address-watcher/ip-address-watcher.log'
 
-print('Starting job: {}'.format(jobListener))
-res = requests.get(jobListener)
+print('Starting job: {}'.format(strJobListener))
+res = requests.get(strJobListener)
 
 print('Status code: {}'.format(res.status_code))
 print('Response body: {}'.format(res.content.decode("utf-8")))
 
-f=open("/home/pi/share/ip-address-watcher/ip-address-watcher.log", "a+")
+f=open(strLogLocation, 'a+')
 f.write("Status code: {}\rResponse body: {}\r\r".format(res.status_code, res.content.decode("utf-8")))
 f.close() 
